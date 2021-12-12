3fdb811eb1504522342156114ccc4722719e3129
kylesbowling2021 <92696125+kylesbowling2021@users.noreply.github.com>
2021-12-11 20:33:23 -0600

    Update fix ugly player.js

--- fix ugly player.js
+++ fix ugly player.js
@@ -2,9 +2,11 @@
 // @name         remove ugly youtube player
 // @version      0.1
 // @description  cope sneed buck chuck
-// @author       You
+// @author       Kyle
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @downloadURL  https://github.com/kylesbowling2021/uglyplayer/edit/main/fix%20ugly%20player.js
+// @installURL   https://github.com/kylesbowling2021/uglyplayer/edit/main/fix%20ugly%20player.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
